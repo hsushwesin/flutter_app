9da8f03401a2767c15a9c4921fc4f6a0433e359b
hsushwesin <hsushwesinit@gmail.com>
2024-02-14 13:52:45 +0630

    Create: Register Screen Page

---
+++ flutter_app/lib/Screens/register_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../rounded_button.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _email = '';
+  String _password = '';
+  String _name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          'Registration',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Name',
+              ),
+              onChanged: (value) {
+                _name = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'Create Account',
+              onBtnPressed: () => null,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

1a8260b3ae606daaed45de05628cad555d5d3228
hsushwesin <hsushwesinit@gmail.com>
2024-02-15 11:01:07 +0630

    Fix:logout

--- flutter_app/lib/Screens/home_screen.dart
+++ flutter_app/lib/Screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/Services/auth_services.dart';
-import 'login_screen.dart'; 
+import 'login_screen.dart';
 import 'package:http/http.dart' as http;
+
+// Import your authentication service
+import 'package:flutter_app/Services/auth_services.dart';
+
 class Globals {
   static String? authToken;
 }
@@ -9,31 +12,6 @@ class Globals {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  Future<void> _logout(BuildContext context) async {
-    // Fetch the authentication token from wherever it's stored
-    String? authToken = Globals.authToken; // Assuming authToken is stored globally, replace this with your actual logic
-
-    if (authToken != null) {
-      // Call the logout function from AuthServices
-      http.Response response = await AuthServices.logout(authToken);
-
-      if (response.statusCode == 200) {
-        // Logout successful, navigate to login screen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => LoginScreen()),
-        );
-      } else {
-        // Handle error here
-        print('Logout failed: ${response.statusCode}');
-        // You can show an error message or handle the error in any other way
-      }
-    } else {
-      // Handle case where authToken is null
-      print('Authentication token is null');
-      // You can show an error message or handle the case in any other way
-    }
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +21,7 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              _logout(context); // Call the logout function when IconButton is pressed
+              // _logout(context); // Call the logout function when IconButton is pressed
             },
             icon: const Icon(Icons.logout),
           ),
--- flutter_app/lib/Services/auth_services.dart
+++ flutter_app/lib/Services/auth_services.dart
@@ -37,16 +37,4 @@ class AuthServices {
     print(response.body);
     return response;
   }
-
-  static Future<http.Response> logout(String? authToken) async {
-    var url = Uri.parse(baseURL + 'auth/logout');
-    http.Response response = await http.post(
-      url,
-      headers: {
-        'Authorization': 'Bearer $authToken', 
-      },
-    );
-    return response;
-  }
-
 }
